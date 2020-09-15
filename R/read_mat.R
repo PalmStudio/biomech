e37fb696ebf3aab535ed495f936b2a7ddffbbfcf
remi.vezy <VEZY@users.noreply.github.com>
2020-09-15 11:14:20 +0200

    Update read_mat.R

--- R/read_mat.R
+++ R/read_mat.R
@@ -4,7 +4,21 @@
 #'
 #' @param path Path to the file
 #'
-#' @return A formatted [data.frame()]
+#' @return A formatted [data.frame()], with each row being a point and with colums being:
+#' - distance (m): distance between the previous point and this point (first value should be positive)
+#' - type: section type. 1: triangle (bottom-oriented); 2 : rectangle; 3 : triangle (top-oriented);
+#' 4 : ellipsis; 5 : circle.
+#' - width (m): section width
+#' - height (m): section height
+#' - inclination (degree): insertion angle (vertical)
+#' - torsion (degree): torsion angle
+#' - x: x coordinate
+#' - y: y coordinate
+#' - z: z coordinate
+#' - mass (kg): mass of the section at the point
+#' - mass_right (kg): mass carried by the object, on the right side
+#' - mass_left (kg): mass carried by the object, on the left side
+#'
 #' @export
 #'
 #' @examples
@@ -19,7 +33,7 @@ read_mat = function(path){
   }
   df = data.frame(t(matExp))
 
-  colnames(df) = c("distance","type","width","heigth","inclination",
+  colnames(df) = c("distance","type","width","height","inclination",
                    "torsion","x","y","z","mass","mass_right",
                    "mass_left")
   df
