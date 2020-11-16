ade7679749fee81c0506ecd8e16f001c44167ebe
remi.vezy <VEZY@users.noreply.github.com>
2020-11-16 15:50:26 +0100

    Refine optimization
    Add several starting points + a plot to see the sensitivity of the optimized value to the starting points

--- R/optimize_bend.R
+++ R/optimize_bend.R
@@ -5,16 +5,27 @@
 #' observations.
 #'
 #' @param field_data      Field data (data.frame of x, y and z coordinates of the points)
-#' @param elastic_modulus Elasticity modulus (bending, MPa) vector: c(min,start,max)
-#' @param shear_modulus   Shear modulus (torsion, MPa) vector: c(min,start,max)
+#' @param elastic_modulus Elasticity modulus (bending, MPa). See details.
+#' @param shear_modulus   Shear modulus (torsion, MPa). See details.
 #' @param type            Type of optimization (either "bending","torsion" or "all")
 #' @param tol             Tolerance for optimization accuracy
+#' @param nb_rep          Number of starting points for the optimization algorithm
+#' @param method          Method to use when optimizing one parameter (see [stats::optim()])
 #' @param ...             Further parameters to pass to [bend()]
 #'
-#' @details The minimim and maximum values are not used when a parameter is not
-#' optimized, but they still should be provided by the user though.
+#' @details The elastic_modulus and shear_modulus are either provided as a vector
+#' of min and max values if optimized, or as a single value to force the parameter if
+#' not optimized.
 #'
-#' @return The optimized values
+#' @return A list of the outputs:
+#' - The best optimized values for each parameter
+#' - `init_values`: a data.frame with the initial values used for each repetition
+#' - `optim_values`: a data.frame with the optimized values for each step
+#' - `min_quadratic_error`: minimum quadratic error of all repetitions
+#' - `rep_min_crit`: index of the repetition that gave the minimum quadratic error
+#' - `plots`: plots of optimal value ~ initial value for each parameter to analyze
+#'the sensitivity of the optimized value to the starting points.
+#'
 #' @export
 #'
 #' @examples
@@ -26,11 +37,16 @@
 #' # [1] 1209.396
 #' # $shear_modulus
 #' # [1] 67.44096
+#'
+#' # If only the elastic_modulus is optimized, the shear_modulus has to be fixed:
+#' #' # optimize_bend(field_data, shear_modulus = 100, type = "bending")
 optimize_bend = function(field_data,
-                         elastic_modulus = c(1,100,10000),
-                         shear_modulus = c(1,1000,10000),
+                         elastic_modulus = c(1,10000),
+                         shear_modulus = c(1,10000),
                          type = c("all","bending","torsion"),
+                         method = "L-BFGS-B",
                          tol = .Machine$double.eps^0.25,
+                         nb_rep = 5,
                          ...){
 
   type = match.arg(type, c("all","bending","torsion"), several.ok = FALSE)
@@ -53,45 +69,110 @@ optimize_bend = function(field_data,
   if(type != "all"){
     # univariate optimization:
     if(type == "bending"){
-      interval = elastic_modulus[-2]
-      y = elastic_modulus[2]
+      interval = elastic_modulus
+      start_values = data.frame(elastic_modulus =
+                                  stats::runif(n = nb_rep, min = elastic_modulus[1],
+                                               max = elastic_modulus[2]))
+      y = shear_modulus[1]
       param_name = "elastic_modulus"
     }else{
-      interval = shear_modulus[-2]
-      y = shear_modulus[2]
+      interval = shear_modulus
+      start_values = data.frame(shear_modulus =
+                                  stats::runif(n = nb_rep, min = shear_modulus[1],
+                                               max = shear_modulus[2]))
+      y = elastic_modulus[1]
       param_name = "shear_modulus"
     }
 
+    optim = vector("list",nb_rep)
 
-    out_opti= stats::optimize(f = compute_error,
-                              interval= interval,
-                              y = y,
-                              field_data = field_data,
-                              unbent_data = df_unbent,
-                              type = type,
-                              verbose = FALSE,
-                              ...,
-                              tol = tol)
-    params= as.list(out_opti$minimum)
-    names(params)= param_name
+    for (irep in seq_len(nb_rep)){
+      if (method=="L-BFGS-B" || method=="Brent") {
+        try(optim[[irep]] <- stats::optim(par = as.numeric(start_values[irep,]),
+                                          fn = compute_error,
+                                          method = method,
+                                          lower = interval[1],
+                                          upper = interval[2],
+                                          control = list(reltol = tol),
+                                          y = y,
+                                          field_data = field_data,
+                                          unbent_data = df_unbent,
+                                          type = type,
+                                          verbose = FALSE,
+                                          ...))
+      } else {
+        try(optim[[irep]] <- stats::optim(par = as.numeric(start_values[irep,]),
+                                          fn = compute_error,
+                                          method = method,
+                                          control = list(reltol = tol),
+                                          y = y,
+                                          field_data = field_data,
+                                          unbent_data = df_unbent,
+                                          type = type,
+                                          verbose = FALSE,
+                                          ...))
+      }
+    }
+
   }else{
-    # multivariate optimization:
-    out_opti= dfoptim::nmkb(fn= compute_error,
-                            par= c(elastic_modulus[2],
-                                   shear_modulus[2]),
-                            lower= c(elastic_modulus[1],
-                                     shear_modulus[1]),
-                            upper= c(elastic_modulus[3],
-                                     shear_modulus[3]),
-                            y = NULL,
-                            field_data = field_data,
-                            unbent_data = df_unbent,
-                            type = type,
-                            verbose = FALSE,
-                            ...)
-    params= as.list(out_opti$par)
-    names(params)= c("elastic_modulus","shear_modulus")
+    param_name = c("elastic_modulus", "shear_modulus")
+    start_values =
+      data.frame(elastic_modulus =
+                   stats::runif(n = nb_rep, min = elastic_modulus[1],
+                                max = elastic_modulus[2]),
+                 shear_modulus =
+                   stats::runif(n = nb_rep, min = shear_modulus[1],
+                                max = shear_modulus[2]))
+
+    optim = vector("list",nb_rep)
+
+    for (irep in seq_len(nb_rep)){
+      # multivariate optimization:
+      try(optim[[irep]] <- dfoptim::nmkb(fn= compute_error,
+                                         par= as.numeric(start_values[irep,]),
+                                         lower= c(elastic_modulus[1],
+                                                  shear_modulus[1]),
+                                         upper= c(elastic_modulus[2],
+                                                  shear_modulus[2]),
+                                         y = NULL,
+                                         field_data = field_data,
+                                         unbent_data = df_unbent,
+                                         type = "all",
+                                         verbose = FALSE,
+                                         ...)
+      )
+    }
   }
+
+  # Get the estimated values
+  est_values = t(rbind(sapply(optim,`[[`,"par")))
+
+  # Which repetition has the smallest criterion
+  ind_min_crit = which.min(sapply(optim, function(x) {
+    if (!is.null(x$value)) x$value
+  }))
+
+  params = as.list(est_values[ind_min_crit,])
+  names(params) = param_name
+  params$init_values = start_values
+  params$optim_values = est_values
+  params$min_quadratic_error = optim[[ind_min_crit]]$value
+  params$rep_min_crit = ind_min_crit
+
+  p =
+    lapply(seq_along(param_name), function(x){
+      data.frame(start_values = start_values[,x],
+                 est_values = est_values[,x])%>%
+        ggplot2::ggplot(ggplot2::aes(x = .data$start_values,
+                                     y = .data$est_values))+
+        ggplot2::geom_point()+
+        ggplot2::xlab("Initial value")+
+        ggplot2::ylab("Estimated")+
+        ggplot2::ggtitle(paste("Optimized parameter: ",param_name[x]))+
+        ggplot2::theme(aspect.ratio=1)
+    })
+
+  params$plots = p
   return(params)
 }
 
